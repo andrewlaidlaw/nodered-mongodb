efbbae2f330e7c2bf0c6f59c5ae89d6ec44572d2
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-03-19 17:58:46 +0000

    Environment variables

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const os = require("os");
 var mongo = require('mongodb'); 
 var MongoClient = require('mongodb').MongoClient;
 
-const mongoHost = process.env.database-host;
-const mongoPort = process.env.database-port;
-const mongoDatabase = process.env.database-name;
-const mongoUser = process.env.database-user;
-const mongoPassword = process.env.database-password;
+// Collect database settings
+const mongoHost = process.env.database_host;
+const mongoPort = process.env.database_port;
+const mongoDatabase = process.env.database_name;
+const mongoUser = process.env.database_user;
+const mongoPassword = process.env.database_password;
 
+// Build MongoDB connection string
 var mongourl = "mongodb://" + mongoHost + ":" + mongoPort + "/" + mongoDatabase
 
 const app = express();
@@ -17,6 +19,7 @@ const port = 8080;
 const hostname = "0.0.0.0";
 
 app.get('/', (req, res) => {
+    // You should only see this if something has gone wrong
     output = "Bollocks";
     mongoConnect();
   
