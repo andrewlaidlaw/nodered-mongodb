666b917519c19bda0b56f6abad4544fe66a88a8f
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-06-24 13:20:53 +0100

    Adding parse elements to query

--- node-js/app.js
+++ node-js/app.js
@@ -12,50 +12,27 @@ const mongoUser = process.env.database_user;
 const mongoPassword = process.env.database_password;
 
 // Build MongoDB connection string
+
+// Used for OpenShift environment
 var url = "mongodb://" + mongoUser + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDatabase
-// var url = "mongodb+srv://" + mongoUser + ":" + mongoPassword + "@" + mongoHost + "/" + mongoDatabase
+// Used for local testing
 // var url = "mongodb://localhost:27017/performance"
 
 const app = express();
 const port = 8080;
 const hostname = "0.0.0.0";
 
-app.get('/', (req, res) => {
-    /* MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(mongoDatabase)
-        var result =  dbo.collection("performance").findOne({}, function(err,result) {
-            if (err) throw err;
-            console.log(result);
-            // res.send(result);
-        });
-        db.close(); 
-    }); */
-    res.send(req.query);
-})
+function integise(query) {
+    if (query.model) query.model = parseInt(query.model);
+    if (query.sockets) query.sockets = parseInt(query.sockets);
+    if (query.coresPerSocket) query.coresPerSocket = parseInt(query.coresPerSocket);
+    if (query.totalCores) query.totalCores = parseInt(query.totalCores);
+    if (query.frequencyGHz) query.frequencyGHz = parseFloat(query.frequencyGHz);
+    return query;
+}
 
-app.get('/connect', (req, res) => {
-    const client = new MongoClient(url);
-    async function run() {
-      try {
-        await client.connect();
-        const database = client.db(mongoDatabase);
-        const collection = database.collection("performance");
-        var queryString = req.query;
-        console.log(queryString);
-        collection.find(queryString).toArray(function(error, results) {
-            if (error) throw error;
-            console.log(results);
-            res.send(results);
-        });
-        //res.json(result);
-      } catch (e) {
-        console.log("Error: " + e);
-      } finally {
-        await client.close();
-      }
-    }
-    run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send("ok");
 })
 
 app.get('/findall', (req, res) => {
@@ -69,6 +46,7 @@ app.get('/findall', (req, res) => {
             console.log("connected");
             const collection = client.db(mongoDatabase).collection("performance");
             console.log("collection set");
+            findQuery = integise(findQuery);
             console.log("query is: " + JSON.stringify(findQuery));
             result = await collection.find(findQuery).toArray();
             console.log("search completed");
@@ -93,7 +71,7 @@ app.get('/url', (req, res) => {
 })
 
 app.get('/test', (req, res) => {
-    var output = "This is the value: " + req.query.value;
+    var output = "This is the value: " + JSON.stringify(req.query);
     res.send(output);
 })
 
